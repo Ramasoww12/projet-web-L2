be59b730cd136f7461f3e67aa6bef13d77c9e2a9
Ramasoww12 <sowrama807@gmail.com>
2025-04-24 17:04:11 +0200

    Add files via upload

---
+++ 404.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <title>Page non trouvée</title>
+    <link rel="stylesheet" href="/assets/css/style.css"> <!-- si tu as un style -->
+</head>
+<body>
+    <h1>404 - Page non trouvée</h1>
+    <p>Désolé, la page que vous cherchez n'existe pas.</p>
+    <a href="/accueil">Retour à l'accueil</a>
+</body>
+</html>
